bbeb71fc567b1e1c1d57ce527e1459d0c6b8254b
Eduardo Schettino <schettino72@gmail.com>
2022-04-24 22:14:58 +0800

    pyflakes: read config INI files. Support 'ignore' flake8 codes.

--- src/pydevtool/lint_pyflakes.py
+++ src/pydevtool/lint_pyflakes.py
@@ -1,18 +1,85 @@
 import re
+from configparser import RawConfigParser
 
+from doit.exceptions import TaskFailed
 from rich.panel import Panel
-
 from pyflakes.api import checkPath
 from pyflakes.reporter import Reporter as FlakeReporter
 
 
+# based on flake8 4.0:  src/flake8/plugins/pyflakes.py
+FLAKE8_PYFLAKES_CODES = {
+    "F401": "UnusedImport",
+    "F402": "ImportShadowedByLoopVar",
+    "F403": "ImportStarUsed",
+    "F404": "LateFutureImport",
+    "F405": "ImportStarUsage",
+    "F406": "ImportStarNotPermitted",
+    "F407": "FutureFeatureNotDefined",
+    "F501": "PercentFormatInvalidFormat",
+    "F502": "PercentFormatExpectedMapping",
+    "F503": "PercentFormatExpectedSequence",
+    "F504": "PercentFormatExtraNamedArguments",
+    "F505": "PercentFormatMissingArgument",
+    "F506": "PercentFormatMixedPositionalAndNamed",
+    "F507": "PercentFormatPositionalCountMismatch",
+    "F508": "PercentFormatStarRequiresSequence",
+    "F509": "PercentFormatUnsupportedFormatCharacter",
+    "F521": "StringDotFormatInvalidFormat",
+    "F522": "StringDotFormatExtraNamedArguments",
+    "F523": "StringDotFormatExtraPositionalArguments",
+    "F524": "StringDotFormatMissingArgument",
+    "F525": "StringDotFormatMixingAutomatic",
+    "F541": "FStringMissingPlaceholders",
+    "F601": "MultiValueRepeatedKeyLiteral",
+    "F602": "MultiValueRepeatedKeyVariable",
+    "F621": "TooManyExpressionsInStarredAssignment",
+    "F622": "TwoStarredExpressions",
+    "F631": "AssertTuple",
+    "F632": "IsLiteral",
+    "F633": "InvalidPrintSyntax",
+    "F634": "IfTuple",
+    "F701": "BreakOutsideLoop",
+    "F702": "ContinueOutsideLoop",
+    "F703": "ContinueInFinally",
+    "F704": "YieldOutsideFunction",
+    "F705": "ReturnWithArgsInsideGenerator",
+    "F706": "ReturnOutsideFunction",
+    "F707": "DefaultExceptNotLast",
+    "F721": "DoctestSyntaxError",
+    "F722": "ForwardAnnotationSyntaxError",
+    "F723": "CommentAnnotationSyntaxError",
+    "F811": "RedefinedWhileUnused",
+    "F812": "RedefinedInListComp",
+    "F821": "UndefinedName",
+    "F822": "UndefinedExport",
+    "F823": "UndefinedLocal",
+    "F831": "DuplicateArgument",
+    "F841": "UnusedVariable",
+    "F901": "RaiseNotImplemented",
+}
+
 class FlakeRichReporter(FlakeReporter):
-    def __init__(self, console):
+    def __init__(self, console, ignore, convert_flake8_code=False):
         self.print = console.print
+        self.convert_flake8_code = convert_flake8_code
+        if convert_flake8_code:
+            self.ignore = set()
+            for key in ignore:
+                if item:=FLAKE8_PYFLAKES_CODES.get(key):
+                    self.ignore.add(item)
+                else:
+                    self.ignore.add(key)
+        else:
+            self.ignore = set(ignore)
+        self.reported = 0
 
     def flake(self, msg):
+        if msg.__class__.__name__ in self.ignore:
+            return
+        self.reported += 1
         text = msg.message % msg.message_args
-        self.print(f'{msg.filename}:{msg.lineno}:{msg.col+1} {text}')
+        self.print(f'{msg.filename}:{msg.lineno}:{msg.col+1} {msg.__class__.__name__} - {text}')
 
     def syntaxError(self, filename, msg, lineno, offset, text):
         """
@@ -52,10 +119,24 @@ class FlakeRichReporter(FlakeReporter):
 
 class LintPyflakes():
     """pyflakes"""
-    def __init__(self, console):
-        self.flake_reporter = FlakeRichReporter(console)
+    def __init__(self, console, config_file=None, config_section="flake8", convert_flake8_code=False):
+        ignore = []
+        if config_file:
+            config = RawConfigParser()
+            config.read(config_file)
+            if config.has_section(config_section):
+                if ignore_str := config[config_section].get('ignore'):
+                    for code in ignore_str.split(','):
+                        code = code.strip()
+                        if code and code[0] == 'F':  # ignore codestyle
+                            ignore.append(code)
+        self.flake_reporter = FlakeRichReporter(console, ignore=ignore,
+                                                convert_flake8_code=convert_flake8_code)
 
     def __call__(self, fn):
         """execute pyflakes on a single file"""
-        flake_result = checkPath(fn, reporter=self.flake_reporter)
-        return flake_result == 0
+        # checkPath() returns total flakes including ignored
+        self.flake_reporter.reported = 0
+        checkPath(fn, reporter=self.flake_reporter)
+        if(self.flake_reporter.reported != 0):
+            return TaskFailed('Check failed', report=True)
